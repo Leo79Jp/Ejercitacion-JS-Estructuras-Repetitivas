4b8b5bbde13e9279bbfdd6d22f276e6c801f2981
Leo79Jp <leojparodi@gmail.com>
2024-07-06 21:55:11 -0300

    actualize y comprendi el ejercicio 01

--- ejercicio01.js
+++ ejercicio01.js
@@ -1,17 +1,41 @@
 // Ejercicio 1
 // Hacer los algoritmos que calculen el promedio de una serie de notas hasta que se ingrese una nota
 // negativa, utilizar las tres estructuras repetitivas.
-const prompt = require('prompt-sync')({ siging: true})
+const prompt = require('prompt-sync')({ siging: true }) // Paso adicional: npm i prompt-sync
 
-let nota
-let suma = 0
-let cantidad = 0
-let promedio
+// let nota = 0
+// let suma = 0
+// let cantidad = 0
+// do {
+//   cantidad++
+//   suma += nota
+//   nota = prompt(`Ingrese la nota N° ${cantidad}: `) // alt+96 -> `
+//   nota = parseInt(nota) // Convertir lo leido en string a integer
+// } while (nota >= 0)
+//     console.log(`El promedio es: ${suma / --cantidad}`)
 
-do {
-    cantidad++
-    nota = prompt(`Ingrese la nota Nº ${cantidad}:`) //alt 96
-    nota = parseInt(nota)
+/* while (nota >= 0) {
+  nota = parseInt(prompt(`Ingrese la nota N° ${++cantidad}: `)) 
+  if (nota >= 0) {
     suma += nota
-    console.log({nota});
-} while (nota >= 0);
+  }
+} */
+
+
+// ciclo for - no se usa para hacer este tipo de consulta cuando no sabes cuantos ciclos tiene que ,hacer
+// for (let nota = suma = cantidad = 0; nota >= 0;) {
+//   suma += nota
+//   nota = parseInt(prompt(`Ingrese la nota N° ${++cantidad}: `))
+// }
+let nota = 0
+let cantidad = 0
+let suma = 0
+
+do{
+    cantidad++
+    suma +=  nota
+    nota = parseInt(prompt(`Ingrese la nota Nº ${cantidad}: `))
+} while(nota >= 0); 
+
+console.log(suma / --cantidad);
+

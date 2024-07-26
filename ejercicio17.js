001b84b9b6402d6b1585cffed3bd4189f3196b28
Leo79Jp <leojparodi@gmail.com>
2024-07-26 11:54:18 -0300

    complete ejercicio 17

--- ejercicio17.js
+++ ejercicio17.js
@@ -2,3 +2,23 @@
 // algoritmo que le permita registrar el tiempo empleado en cada recorrido, al final del proceso el
 // algoritmo debería mostrar el promedio de los tiempos empleados. También necesita saber cuántas
 // veces su tiempo fue menor a valor dado y cuál fue su mejor tiempo realizado.
+const kmRecorrido = 200
+const min = 30 // kilometros por hora minimos (Velocidad)
+const max = 50 // kilometros por hora maximos (Velocidad)
+let mejorTiempo = 10000
+let promedio =0
+let tiempoDiario 
+
+    for (let i = 1; i <= 5; i++){
+        const kmHora = Math.floor(Math.random() * (max - min)) + min;
+        tiempoDiario = kmRecorrido * 60 / kmHora
+        console.log( `A una velocidad de ${kmHora}km/h, el tiempo empleado fue de ${Math.floor(tiempoDiario)} minutos en realizar los ${kmRecorrido}km`);
+        promedio+=tiempoDiario
+        if( mejorTiempo > tiempoDiario){
+            mejorTiempo = tiempoDiario
+        }
+    }
+    console.log(`El promedio de los tiempos empleados fue de ${Math.floor(promedio/5)} minutos`);
+    console.log(`El mejor tiempo en realizar los ${kmRecorrido}km fue de ${Math.floor(mejorTiempo)} minutos`);
+
+

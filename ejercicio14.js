37f2488ee25d95674e2be33e4d653e6919028073
Leo79Jp <leojparodi@gmail.com>
2024-07-26 02:18:31 -0300

    complete el ejercicio 15

--- ejercicio14.js
+++ ejercicio14.js
@@ -1,3 +1,58 @@
 // 14. Leer 10 valores desde teclado y mostrar la media de los pares y la media de los impares. Hacer tres
 // versiones, con un bucle: para, mientras y repetir. Repetir el ejercicio considerando que el número de
 // valores se le solicita al usuario.
+const prompt = require('prompt-sync')({ siging: true }) 
+let pares = 0
+let cantPares = 0
+let impares = 0
+let cantImpares = 0
+let index = 0
+
+/*  ------------------------- Ciclo For ------------------------ */
+// for (let i = 1; i <= 10; i++) {
+//     valorIngresado = parseInt(prompt(`Ingrese un numero: `))
+//     if ((valorIngresado%2) == 0) {
+//         pares+= valorIngresado
+//         cantPares++
+//     }
+//     if ((valorIngresado%2) !== 0) {
+//         impares+= valorIngresado
+//         cantImpares++
+//     }
+// }
+// console.log(`La media de los numeros pares es: ${pares / cantPares}`);
+// console.log(`La media de los numeros Impares es: ${impares / cantImpares}`);
+
+/*  ------------------------- Ciclo do While ------------------------ */
+
+// do {
+//     valorIngresado = parseInt(prompt(`Ingrese un numero: `))
+//     if ((valorIngresado%2) == 0) {
+//         pares+= valorIngresado
+//         cantPares++
+//     }
+//     if ((valorIngresado%2) !== 0) {
+//         impares+= valorIngresado
+//         cantImpares++
+//     }
+//     index++
+// } while (index != 10);
+// console.log(`La media de los numeros pares es: ${pares / cantPares}`);
+// console.log(`La media de los numeros Impares es: ${impares / cantImpares}`);
+
+/*  ------------------------- Ciclo  While ------------------------ */
+
+while (index !=10) {
+    valorIngresado = parseInt(prompt(`Ingrese un numero: `))
+    if ((valorIngresado%2) == 0) {
+        pares+= valorIngresado
+        cantPares++
+    }
+    if ((valorIngresado%2) !== 0) {
+        impares+= valorIngresado
+        cantImpares++
+    }
+    index++
+}
+console.log(`La media de los numeros pares es: ${pares / cantPares}`);
+console.log(`La media de los numeros Impares es: ${impares / cantImpares}`);
--- ejercicio15.js
+++ ejercicio15.js
@@ -4,3 +4,25 @@
 // • No múltiplo de 5.
 // • Mayor que 100.
 // • Menor que 10.000.
+
+const prompt = require('prompt-sync')({ siging: true }) 
+let resultado = 1
+do {
+    valorIngresado = parseInt(prompt(`Ingrese un numero: `))
+    if (valorIngresado>100){
+        console.log('Es mayor a 100');
+        if (valorIngresado<10000){
+            console.log('Es menor que 10000');
+            if(valorIngresado%5 != 0){
+                console.log('No es multiplo de 5');
+                if (valorIngresado%2 == 0){
+                    console.log('Es multiplo de 2');
+                    console.log('Valor correcto');
+                    resultado = 0
+                }
+            }
+        }
+    }
+
+} while ( resultado == 1 );
+
